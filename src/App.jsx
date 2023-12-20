dca6009bbecc2d8854deb4fd8f4f930adce2360a
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2023-12-20 22:01:47 +0530

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,50 @@
 import { useState } from "react"
 import "./App.css"
+import NewTodoForm from "./NewTodoForm"
+import TodoList from "./TodoList"
 
 export default function App() {
-  const [newItem,setNewItem]=useState("")
+ 
   const [todos,setTodos]=useState([])
   
-  function handleSubmit(e){
-    e.preventDefault()
+   function addTodo(title){
+    return 
+    setTodos(currentTodos =>{
+        return [
+          ...currentTodos,
+          {
+            id: crypto.randomUUID(),title,
+            completed:false
+          }
+        ]
+      })
+   }
 
-     
+  function toggleTodo(id,completed){
+    setTodos(currentTodos =>{
+      return currentTodos.map(todo=>{
+        if(todo.id===id){
+          return{
+            ...todo,completed
+          }
+        }
+      })
+    })
+  }
+
+
+
+  function deleteTodo(id){
+    setTodos(currentTodos=>{
+      return currentTodos.filter(todo => todo.id !=id )
+    })
   }
   return (
 
     <>
-      <form 
-      onSubmit={handleSubmit}
-      className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input 
-          value={newItem}
-          onChange={e => setNewItem(e.target.value)}
-          type="text" id="item"></input>
-          <button className="btn">Add</button>
-        </div>
-      </form>
-      <h1>Todo List</h1>
-      <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-               Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-      </ul>
+     <NewTodoForm onSubmit={addTodo}/>
+      <h1 className="header">Todo List</h1>
+     <TodoList />
     </>
   )
 }
